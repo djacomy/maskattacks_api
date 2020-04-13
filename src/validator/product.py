15971a062bbd772a859690c7d6d9537f7048ea8a
Damien JACOMY <damien.jacomy@gmail.com>
2020-04-13 18:04:19 +0200

    Add required to validator.

--- src/validator/product.py
+++ src/validator/product.py
@@ -2,9 +2,9 @@ from marshmallow import Schema, fields
 
 
 class ProductEquivalenceRequest(Schema):
-    reference = fields.String()
-    count = fields.Integer()
+    reference = fields.String(required=True)
+    count = fields.Integer(required=True)
 
 
 class ProductEquivalenceList(Schema):
-    materials = fields.Nested(ProductEquivalenceRequest, many=True)
+    materials = fields.Nested(ProductEquivalenceRequest, many=True, required=True)
